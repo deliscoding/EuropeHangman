1d87a449f3b7e11461d757e18f3ccad0b618aebb
deliscoding <danelis19@gmail.com>
2018-07-09 05:11:14 -0400

    added setupGame and restartGame to JS file

--- assets/javascript/hangmanGame.js
+++ assets/javascript/hangmanGame.js
@@ -24,4 +24,32 @@ var hangmanGame = {
   letterGuessed: null,
   wins: 0,
 
+  //The setup game method is called when the page first loads
+
+  setupGame: function () {
+    //Pick a random word
+    var objKeys = Object.keys(this.wordsToPick);
+    this.wordInPlay = objKeys[Math.floor(Math.random() * objKeys.length)];
+    //Log the random country to the console
+    console.log(this.wordInPlay);
+    //Split the chosen word into its individaul letters
+    this.lettersOfTheWord = this.wordInPlay.split("");
+  },
+
+  //Function that restarts the game by resetting all the variables
+  restartGame: function () {
+    document.querySelector("#guessed-letters").innerHTML = "";
+    this.wordInPlay = null;
+    this.lettersOfTheWord = [];
+    this.matchedLetters = [];
+    this.guessedLetters = [];
+    this.guessesLeft = 0;
+    this.totalGuesses = 0;
+    this.letterGuessed = null;
+    this.setupGame();
+  },
+
+
 }
+//Initialize the game when the page loads
+hangmanGame.setupGame();
